88ef93cfff032b59e218a39c6c4fe21829ac5848
maxlyara1 <maks.lyara@gmail.com>
2024-10-11 21:08:36 +0300

    not needed imports deleted

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI, File, UploadFile, Form, Request
 from fastapi.responses import FileResponse, HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import pandas as pd
-from io import BytesIO
 import uvicorn
 import os
 
