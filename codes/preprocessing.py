293fad53278dc70b2dedd62eb4b9b59f37401efb
maxlyara1 <maks.lyara@gmail.com>
2024-10-12 20:24:12 +0300

    upd

--- codes/preprocessing.py
+++ codes/preprocessing.py
@@ -2,5 +2,10 @@ import os
 import pandas as pd
 from docx import Document
 
-class PreprocessData:
+class PreprocessUseCase:
+    def creating_dataframe()
+    
+
+
+class PreprocessRegulation:
     def 
